4f33bae1618993df6540210471e5e05b08fc16c6
andrei <afonuk@mail.ru>
2023-02-13 21:45:07 +0300

    Add task5_2.py

--- task2_1.py
+++ task2_1.py
@@ -16,23 +16,3 @@ else:
     n / 2      
     print(min)
 
-# 3.2[18]: Требуется найти в списке целых чисел самый близкий по величине элемент к заданному числу X.
-#  Пользователь вводит это число с клавиатуры, список можно считать заданным. Введенное число не обязательно содержится в списке.
-# Примеры/Тесты:
-# Input: [10, 5, 7, 3, 3, 2, 5, 7, 3, 8], X = 0
-# Output: 2
-# Input: [10, 5, 7, 3, 3, 2, 5, 7, 3, 8], X = 9
-
-list = [10, 5, 7, 3, 3, 2, 5, 7, 3, 8]
-x = input("Введите число:")
-x = int(x)
-# max = list[0]
-# print(min(list))
-new_set = set(list)
-print(new_set)
-# for i in range(0,len(list)):
-#     if list[i] > x:        
-#         list.remove([i])
-#         print(i)
-# list = [i for i in list if i > min(list) and i < max(list)]
-# print(list[x])
